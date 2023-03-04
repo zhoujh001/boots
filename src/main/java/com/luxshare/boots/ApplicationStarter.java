0aacbe8660023ae19222322426e6375095a4d50c
zhoujh <599245299@qq.com>
2023-03-04 21:05:43 +0800

    初始化项目

---
+++ src/main/java/com/luxshare/boots/ApplicationStarter.java
@@ -0,0 +1,15 @@
+package com.luxshare.boots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujh
+ * @date 2023/3/4
+ **/
+@SpringBootApplication
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
